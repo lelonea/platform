6ce793bd9e06ff91fe611b2a9fc2bd3273d7e95a
Nastya <leloneya@gmail.com>
2021-06-02 12:52:57 +0300

    Remade function send_message for sending massages to users from lists

--- instabot/instabot.py
+++ instabot/instabot.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-
+from time import sleep
 from data import username, password
 
 
@@ -49,36 +49,44 @@ class InstagramBot:
         disable_notification.click()
 
     # method for sending message
-    def send_message(self, user='', message=''):
+    def send_message(self, user: list, time: list):
         self.open_direct()
         self.close_notification()
 
-        WebDriverWait(self.browser, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Новое сообщение']")))
-        direct_button = self.browser.find_element_by_css_selector("[aria-label='Новое сообщение']")
-        direct_button.click()
+        user_time_dict = dict(zip(user, time))
 
-        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[placeholder='Поиск...']")))
-        search = self.browser.find_element_by_css_selector("[placeholder='Поиск...']")
-        search.clear()
-        search.send_keys(user)
+        for key in user_time_dict:
+            sleep(1)
+            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Новое сообщение']")))
+            direct_button = self.browser.find_element_by_css_selector("[aria-label='Новое сообщение']")
+            direct_button.click()
 
-        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, f"//*[text()='{user}']")))
-        self.browser.find_element_by_xpath(f"//*[text()='{user}']").click()
+            sleep(1)
+            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[placeholder='Поиск...']")))
+            search = self.browser.find_element_by_css_selector("[placeholder='Поиск...']")
+            search.clear()
+            search.send_keys(key)
 
-        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Далее']")))
-        self.browser.find_element_by_xpath("//*[text()='Далее']").click()
+            sleep(2)
+            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Выбрать / отменить выбор пользователя']")))
+            self.browser.find_element_by_css_selector("[aria-label='Выбрать / отменить выбор пользователя']").click()
 
-        WebDriverWait(self.browser, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "[placeholder='Напишите сообщение...']")))
-        search = self.browser.find_element_by_css_selector("[placeholder='Напишите сообщение...']")
-        search.clear()
-        search.send_keys(message)
+            sleep(1)
+            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Далее']")))
+            self.browser.find_element_by_xpath("//*[text()='Далее']").click()
 
-        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Отправить']")))
-        self.browser.find_element_by_xpath("//*[text()='Отправить']").click()
+            sleep(1)
+            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[placeholder='Напишите сообщение...']")))
+            message = self.browser.find_element_by_css_selector("[placeholder='Напишите сообщение...']")
+            message.clear()
+            message.send_keys(f'Здравствуйте, жду вас завтра в {user_time_dict[key]}. Адрес: Машерова 20. Студия красоты LELO. Вход со стороны проспекта через интернат')
+
+            sleep(1)
+            WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Отправить']")))
+            self.browser.find_element_by_xpath("//*[text()='Отправить']").click()
+            sleep(2)
 
 
 bot = InstagramBot(username, password)
 bot.login()
-bot.send_message('lelolash', 'hello')
+bot.send_message(['lelolash', 'anastasiyalazar'], ['9:00', '12:00'])
