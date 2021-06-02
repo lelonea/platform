8ca81c391faa2b9551c77bc25c2e22370aec6f78
Nastya <leloneya@gmail.com>
2021-06-02 11:56:46 +0300

    Add function for sending message to user

--- instabot/instabot.py
+++ instabot/instabot.py
@@ -1,11 +1,9 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
-import random
+
 from data import username, password
 
 
@@ -40,9 +38,47 @@ class InstagramBot:
 
     # method for opening direct
     def open_direct(self):
-        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR("//*[text()='Messenger']"))))
-        direct_button = self.browser.find_element_by_css_selector("[aria-label=Messenger]")
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Messenger']")))
+        direct_button = self.browser.find_element_by_css_selector("[aria-label='Messenger']")
         direct_button.click()
-        time.sleep(5)
+
+    # method to close notification
+    def close_notification(self):
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Не сейчас']")))
+        disable_notification = self.browser.find_element_by_xpath("//*[text()='Не сейчас']")
+        disable_notification.click()
+
+    # method for sending message
+    def send_message(self, user='', message=''):
+        self.open_direct()
+        self.close_notification()
+
+        WebDriverWait(self.browser, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Новое сообщение']")))
+        direct_button = self.browser.find_element_by_css_selector("[aria-label='Новое сообщение']")
+        direct_button.click()
+
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[placeholder='Поиск...']")))
+        search = self.browser.find_element_by_css_selector("[placeholder='Поиск...']")
+        search.clear()
+        search.send_keys(user)
+
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, f"//*[text()='{user}']")))
+        self.browser.find_element_by_xpath(f"//*[text()='{user}']").click()
+
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Далее']")))
+        self.browser.find_element_by_xpath("//*[text()='Далее']").click()
+
+        WebDriverWait(self.browser, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "[placeholder='Напишите сообщение...']")))
+        search = self.browser.find_element_by_css_selector("[placeholder='Напишите сообщение...']")
+        search.clear()
+        search.send_keys(message)
+
+        WebDriverWait(self.browser, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[text()='Отправить']")))
+        self.browser.find_element_by_xpath("//*[text()='Отправить']").click()
 
 
+bot = InstagramBot(username, password)
+bot.login()
+bot.send_message('lelolash', 'hello')
